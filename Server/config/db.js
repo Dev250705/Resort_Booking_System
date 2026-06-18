c13ba7ce1b2e1519136c979fa5150b8219b55b81
dev <devdalwadi257@gmail.com>
2026-06-18 17:08:42 +0530

    updateee

--- Server/config/db.js
+++ Server/config/db.js
@@ -1,14 +1,40 @@
 const mongoose = require("mongoose");
 
+let cachedConnection = global.mongoose;
+
+if (!cachedConnection) {
+  cachedConnection = global.mongoose = { conn: null, promise: null };
+}
+
 const connectDB = async () => {
-  try {
-  
-    await mongoose.connect(process.env.MONGO_URI);
-  
-    console.log("MongoDB connected");
-  } catch (error) {
-    console.log("DB error:", error.message);
+ 
+  if (cachedConnection.conn) {
+    console.log("MongoDB using cached connection");
+    return cachedConnection.conn;
   }
+
+  if (!cachedConnection.promise) {
+    const opts = {
+      bufferCommands: false,
+      serverSelectionTimeoutMS: 5000, 
+    };
+
+    console.log("MongoDB establishing new connection...");
+    cachedConnection.promise = mongoose.connect(process.env.MONGO_URI, opts).then((mongooseInstance) => {
+      console.log("MongoDB connected successfully");
+      return mongooseInstance;
+    });
+  }
+
+  try {
+    cachedConnection.conn = await cachedConnection.promise;
+  } catch (e) {
+    cachedConnection.promise = null;
+    console.log("DB error:", e.message);
+    throw e;
+  }
+
+  return cachedConnection.conn;
 };
 
 module.exports = connectDB;
